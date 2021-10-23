a0c6f3a4a6e00045883be03044d1bc1615c6b1a5
Nebulion <nebulion@protonmail.com>
2021-10-23 18:52:07 -0400

    Added reset button with grid manipulation

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const body = document.body;
+let grid = 16;
+drawGrid(16);
 
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    document.body.removeChild(document.querySelector('#container'));
     
-    for (let j = 0; j < 16; j++) {
-        const divdiv = document.createElement('div'); 
-        divdiv.addEventListener('mouseenter', () => {
-            divdiv.style.backgroundColor = 'red';
-        }
-        );
-        div.appendChild(divdiv);
-    }
-    body.appendChild(div);
-}
+    grid = prompt("What should the dimensions of this square grid be?", 16);
+    drawGrid(grid);
+})
 
+function drawGrid(grid) {
+    const container = document.createElement('div');
+    container.setAttribute('id', 'container');
+    
+    for (let i = 0; i < grid; i++) {
+        const div = document.createElement('div');
+        for (let j = 0; j < grid; j++) {
+            const divdiv = document.createElement('div');
+            divdiv.addEventListener('mouseenter', () => {
+                divdiv.style.backgroundColor = 'red';
+            }
+            );
+            div.appendChild(divdiv);
+        }
+        container.appendChild(div);
+    }
+    document.body.appendChild(container);
+}
